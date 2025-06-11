012273d1c72a3a1ab1cdcf8ad3902af9525b6277
orangebabu <2409692770@qq.com>
2025-06-11 15:16:44 +0800

    优化代码

--- jinbooks/jinbooks-persistence/src/main/java/com/jinbooks/persistence/service/impl/EmployeeSalarySummaryServiceImpl.java
+++ jinbooks/jinbooks-persistence/src/main/java/com/jinbooks/persistence/service/impl/EmployeeSalarySummaryServiceImpl.java
@@ -173,7 +173,8 @@ public class EmployeeSalarySummaryServiceImpl extends ServiceImpl<EmployeeSalary
 
             BigDecimal amount = switch (matchedEnum) {
                 case COMPANY_COSTS -> summary.getBusinessExpenditureCosts();
-                case SALARY_PAYABLE -> summary.getTotalAmount();
+                case SALARY_PAYABLE -> summary.getTotalAmount().add(summary.getTotalSocialInsurance()).add(summary.getProvidentFund()).add(summary.getPersonalTax());
+                case ACTUAL_SALARY -> summary.getTotalAmount();
                 case PERSONAL_INCOME_TAX -> summary.getPersonalTax();
                 case PERSONAL_WITHHOLDING_SOCIAL_SECURITY -> summary.getTotalSocialInsurance();
                 case PERSONAL_WITHHOLDING_PROVIDENT_FUND -> summary.getProvidentFund();
