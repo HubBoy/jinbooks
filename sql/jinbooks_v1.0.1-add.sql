7903e3a650e36f82cbb9c06674acbb118b551d4e
MaxKey <shimingxy@qq.com>
2025-06-15 21:03:06 +0800

    Create jinbooks_v1.0.1-add.sql

---
+++ sql/jinbooks_v1.0.1-add.sql
@@ -0,0 +1,9 @@
+ALTER TABLE `jinbooks`.`jbx_employee_salary_temp` 
+ADD COLUMN `pay_amount` DECIMAL(10,5) NULL DEFAULT '0.00000' COMMENT '应发工资=工资+应增-应扣' AFTER `personal_tax`;
+
+
+ALTER TABLE `jinbooks`.`jbx_employee_salary` 
+ADD COLUMN `pay_amount` DECIMAL(10,5) NULL DEFAULT '0.00000' COMMENT '应发工资=工资+应增-应扣' AFTER `personal_tax`;
+
+ALTER TABLE `jinbooks`.`jbx_employee_salary_summary` 
+ADD COLUMN `pay_amount` DECIMAL(10,5) NULL DEFAULT '0.00000' COMMENT '应发工资=工资+应增-应扣' AFTER `personal_tax`;
